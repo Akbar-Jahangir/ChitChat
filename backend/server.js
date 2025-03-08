b018c56da3e8f13ce96a910a0a4c37762ea3bd80
Your Name <aliakbar24589@gmail.com>
2025-03-08 16:46:45 +0500

    edited .env

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:3000" }));
-app.use(bodyParser.json());
+app.use(cors({ 
+  origin: process.env.CLIENT_URL || "https://chit-chat-pink.vercel.app"
+}));
 
 const pusher = new Pusher({
   appId: process.env.PUSHER_APP_ID,
