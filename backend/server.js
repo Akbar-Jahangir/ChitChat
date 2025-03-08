7666e77f45d85637204145e3dc6638485759e1c6
Your Name <aliakbar24589@gmail.com>
2025-03-08 15:24:13 +0500

    server port changed 5000 to 8000

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import Pusher from "pusher";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:3000" }));
 app.use(bodyParser.json());
