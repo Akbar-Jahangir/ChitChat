8e0c8e615a0ad620e674339fd157ec4c5117f359
Your Name <aliakbar24589@gmail.com>
2025-03-08 15:40:30 +0500

    defined .css and .png modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,12 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+export default defineConfig(() => {;
 
   return {
     plugins: [react()],
-    define: {
-      'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(env.VITE_SUPABASE_URL),
-      'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(env.VITE_SUPABASE_ANON_KEY),
-    },
+   
     resolve: {
       alias: {
         '@': path.resolve(__dirname, 'src'), // Adjust the alias as needed
