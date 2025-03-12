a94278f569b34d10d27a76255d39e0e749a5b91c
Your Name <aliakbar24589@gmail.com>
2025-03-12 11:58:11 +0500

    1st version

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(cors({
   origin: function(origin, callback) {
     
     const allowedOrigins = [
-      process.env.CLIENT_URL || "https://chit-chat-nine-xi.vercel.app",
+      process.env.CLIENT_URL || "https://chit-chat-dusky-ten.vercel.app",
       "http://localhost:3000",
       "http://localhost:5173"  
     ];
